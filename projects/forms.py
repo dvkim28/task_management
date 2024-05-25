77a858746cf87ff47e7607374545cb72b06b204c
dmitriy kim <d.villarionovich@gmail.com>
2024-05-25 23:06:48 +0200

    Added create view for task; Updated Admin panel for task class

---
+++ projects/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+from projects.models import Task
+
+
+class CreateTaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields = ['name', 'deadline', 'priority', 'assigned_to', 'task_type', 'statuses', 'projects']
+
+    def save(self, commit=True, creator=None):
+        task = super(CreateTaskForm, self).save(commit=False)
+        if creator:
+            task.creator = creator
+        if commit:
+            task.save()
+
--- projects/urls.py
+++ projects/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from projects.views import ProjectDetailView
+from projects.views import ProjectDetailView, TaskCreateView
 
 app_name = 'projects'
 
 urlpatterns = [
     path('<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
+    path('<int:pk>/create', TaskCreateView.as_view(), name='task_create'),
 ]
