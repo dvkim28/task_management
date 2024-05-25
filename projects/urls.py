5e52196cef3d13d629bd2271e5f13ebcccaca1f8
dmitriy kim <d.villarionovich@gmail.com>
2024-05-25 23:37:10 +0200

    Added functionality of creating projects; New feature: when user creates project or task, he automatically becomes a crew/creator of created issue

--- projects/urls.py
+++ projects/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from projects.views import ProjectDetailView, TaskCreateView
+from projects.views import ProjectDetailView, TaskCreateView, ProjectCreateView
 
 app_name = 'projects'
 
 urlpatterns = [
+    path('create/', ProjectCreateView.as_view(), name='project_create'),
     path('<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
     path('<int:pk>/create', TaskCreateView.as_view(), name='task_create'),
 ]
