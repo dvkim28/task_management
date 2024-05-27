89034565870d479a63b9ac519decd35f32a38d0a
dmitriy kim <d.villarionovich@gmail.com>
2024-05-27 16:19:51 +0200

    Added task detail view New model for task comment created

--- projects/forms.py
+++ projects/forms.py
@@ -6,12 +6,12 @@ from projects.models import Task
 class CreateTaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ['name',
+        fields = ['title',
                   'deadline',
                   'priority',
+                  'description',
                   'assigned_to',
                   'task_type',
-                  'statuses',
                   'projects']
 
     def save(self, commit=True, creator=None):
--- projects/urls.py
+++ projects/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from projects.views import ProjectDetailView, TaskCreateView, ProjectCreateView
+from projects.views import ProjectDetailView, TaskCreateView, ProjectCreateView, TaskDetailView
 
 app_name = 'projects'
 
 urlpatterns = [
     path('create/', ProjectCreateView.as_view(), name='project_create'),
     path('<int:pk>/', ProjectDetailView.as_view(), name='project_detail'),
-    path('<int:pk>/create', TaskCreateView.as_view(), name='task_create'),
+    path('create-task/', TaskCreateView.as_view(), name='task_create'),
+    path('task/<int:pk>/', TaskDetailView.as_view(), name='task_detail'),
 ]
