4774385bd1882e1a2c05ca4c9f968678759bfc4a
dmitriy kim <d.villarionovich@gmail.com>
2024-06-13 23:42:22 +0200

    updated settings

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-x=#imgutjl-3k^p^ol&^1%f@6x%k!1&ath@)hbotkfq9v^$7=h")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "0.0.0.0"]
 
 
 
