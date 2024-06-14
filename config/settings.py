8437012f972c23087ec2cc5c88fbeaca1d89dd78
dmitriy kim <d.villarionovich@gmail.com>
2024-06-14 19:36:25 +0200

    Host  updated

--- config/settings.py
+++ config/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
-import dj_database_url
 from pathlib import Path
+
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,19 +85,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://taskmaster_owner:J0kQqHfUnFM3@ep-steep-moon-a56o6z7h.us-east-2.aws.neon.tech/taskmaster?sslmode=require',
+        conn_max_age=600
+    )
 }
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
-DATABASE_URL = "postgresql://taskmaster_owner:J0kQqHfUnFM3@ep-steep-moon-a56o6z7h.us-east-2.aws.neon.tech/taskmaster?sslmode=require"
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
